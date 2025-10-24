356e2af164599922077c71e35dea4c10186c2da4
limonikal <98746926+limonikal@users.noreply.github.com>
2025-10-24 18:08:39 +0300

    Update message-gate.ts

--- src/message-gate.ts
+++ src/message-gate.ts
@@ -197,7 +197,8 @@ export default class MessageGate {
     async #answerToGetTransfer(message: Type.Message) {
         const meta = message.data.meta;
         try {
-            const [result, transfer] = await this.#actions.get(meta.action)!(message.data.data);
+            const value = await this.#actions.get(meta.action)!(message.data.data)
+            const [result, transfer] = value;
             let Transfer = transfer;
             if (transfer !== undefined && !(transfer instanceof Array)) {
                 Transfer = [transfer];
